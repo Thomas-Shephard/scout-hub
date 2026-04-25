aa39a55bb0a27394e23117e57f4934a91047a9b1
Thomas Shephard <thomas@thomas-shephard.com>
2026-04-25 09:51:09 +0100

    chore: reformat repository (#5)

--- src/ScoutHub.Client/src/routes/+page.svelte
+++ src/ScoutHub.Client/src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <div class="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-  <div class="text-center">
-    <h1 class="mb-4 text-5xl font-extrabold tracking-tight text-blue-500">
-      Scout Hub
-    </h1>
-  </div>
+    <div class="text-center">
+        <h1 class="mb-4 text-5xl font-extrabold tracking-tight text-blue-500">Scout Hub</h1>
+    </div>
 </div>
